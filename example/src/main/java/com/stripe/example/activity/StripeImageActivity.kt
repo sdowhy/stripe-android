fb7d8356b377722a0047eab123ce2291b4c035d7
jameswoo-stripe <99316447+jameswoo-stripe@users.noreply.github.com>
2022-11-14 08:04:36 -0800

    Exmaple

--- example/src/main/java/com/stripe/example/activity/StripeImageActivity.kt
+++ example/src/main/java/com/stripe/example/activity/StripeImageActivity.kt
@@ -5,13 +5,24 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.PlaceholderVerticalAlign
+import com.stripe.android.core.networking.StripeResponse
+import com.stripe.android.core.networking.cache.DefaultNetworkCache
 import com.stripe.android.ui.core.R
 import com.stripe.android.uicore.image.StripeImageLoader
 import com.stripe.android.uicore.text.EmbeddableImage
 import com.stripe.android.uicore.text.Html
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class StripeImageActivity : AppCompatActivity() {
     private val LocalStripeImageLoader = staticCompositionLocalOf<StripeImageLoader> {
@@ -27,10 +38,43 @@ class StripeImageActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val httpClient = OkHttpClient.Builder().build()
+        val cache = DefaultNetworkCache(this)
+        val request = Request.Builder().get().url("https://berry-chisel-spinosaurus.glitch.me/content").build()
         super.onCreate(savedInstanceState)
         setContent {
+            val text = remember { mutableStateOf("") }
+            httpClient.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val result = response.body?.string()
+                    cache.get(request.url.toString())?.let {
+                        it.body?.let {
+                            text.value = it
+                        }
+                    } ?: run {
+                        result?.let {
+                            text.value = it
+                            val headers = response.headers.toMultimap()
+                                .mapValues { it.value.joinToString(", ") }
+                                .mapValues { listOf(it.value) }
+                            cache.put(
+                                request.url.toString(),
+                                StripeResponse(
+                                    code = 200,
+                                    body = it,
+                                    headers = headers
+                                )
+                            )
+                        }
+                    }
+                }
+            })
             CompositionLocalProvider(LocalStripeImageLoader provides imageLoader) {
                 Column {
+                    Text(text = text.value)
                     Html(
                         imageLoader = mapOf(
                             "affirm" to EmbeddableImage.Drawable(
